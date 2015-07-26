f6a556883ff75f420e53afde85a7e0c161a72e63
tso <tso@teknik.io>
2015-07-26 11:31:53 -0400

    Made it compile... ¯\_(ツ)_/¯

--- primary_color/median_cut.go
+++ primary_color/median_cut.go
@@ -1,108 +1,116 @@
 package primaryColor
 
+import "math"
+
 func MedianCut(histogram []uint, vbox VBox) (VBox, VBox) {
-  rw := vbox.Rmax - vbox.Rmin
-  gw := vbox.Gmax - vbox.Gmin
-  bw := vbox.Bmax - vbox.Bmin
+	rw := vbox.Rmax - vbox.Rmin
+	gw := vbox.Gmax - vbox.Gmin
+	bw := vbox.Bmax - vbox.Bmin
 
-  boundaryVertices := []uint8{vbox.Rmin, vbox.Rmax, vbox.Gmin, vbox.Gmax, vbox.Bmin, vbox.Bmax}
-  widths := []uint8{rw, gw, bw}
+	boundaryVertices := []uint8{vbox.Rmin, vbox.Rmax, vbox.Gmin, vbox.Gmax, vbox.Bmin, vbox.Bmax}
+	widths := []uint8{rw, gw, bw}
 
-  // If there is a single pixel in the vbox, just return it
-  if (vbox.Count() == 1) {
-    return vbox, nil
-  }
+	// If there is a single pixel in the vbox, just return it
+	if vbox.Count() == 1 {
+		return vbox, vbox
+	}
 
-  total, partialSums, maxIndex := calculatePartialSums(histogram, boundaryVertices, widths)
+	total, partialSums, maxIndex := calculatePartialSums(histogram, boundaryVertices, widths)
 
-  return performCut(partialSums, vbox, boundaryVertices, total, maxIndex)
+	return performCut(partialSums, vbox, boundaryVertices, total, maxIndex)
 }
 
 func calculatePartialSums(histogram []uint, boundaryVertices []uint8, widths []uint8) (uint, []uint, uint) {
-  var maxWidth := 0
-  var maxIndex uint
-  for i, width := range widths {
-    if width > maxWidth {
-      maxWidth = width
-      maxIndex = i
-    }
-  }
+	var maxWidth, width uint8
+	maxWidth = 0
+	var maxIndex uint
+	var i int
+	for i, width = range widths {
+		if width > maxWidth {
+			maxWidth = width
+			maxIndex = uint(i)
+		}
+	}
 
-  markedVertexMin := boundaryVertices[maxIndex*2]
-  markedVertexMax := boundaryVertices[maxIndex*2 + 1]
+	markedVertexMin := boundaryVertices[maxIndex*2]
+	markedVertexMax := boundaryVertices[maxIndex*2+1]
 
-  nonMarkedVertices = append(boundaryVertices[:(i*2)], boundaryVertices[(i*2+1):]...)
-  var histIndex, total uint;
-  partialSums := make([]uint, maxWidth)
-  for i := markedVertexMin; i <= markedVertexMax; i++ {
-    sum := 0
-    for j := nonMarkedVertices[0]; j <= nonMarkedVertices[1]; j++ {
-      for k := nonMarkedVertices[2]; k <= nonMarkedVertices[3]; k++ {
+	nonMarkedVertices := append(boundaryVertices[:(i*2)], boundaryVertices[(i*2+1):]...)
+	var histIndex, total uint
+	partialSums := make([]uint, maxWidth)
+	for i := markedVertexMin; i <= markedVertexMax; i++ {
+		var sum uint
+		sum = 0
+		for j := nonMarkedVertices[0]; j <= nonMarkedVertices[1]; j++ {
+			for k := nonMarkedVertices[2]; k <= nonMarkedVertices[3]; k++ {
 
-        switch maxIndex {
-        case 0:
-          histIndex = ColorIndex(i, j, k)
-        case 1:
-          histIndex = ColorIndex(j, i, k)
-        case 2:
-          histIndex = ColorIndex(j, k, i)
-        }
+				switch maxIndex {
+				case 0:
+					histIndex = ColorIndex(i, j, k)
+				case 1:
+					histIndex = ColorIndex(j, i, k)
+				case 2:
+					histIndex = ColorIndex(j, k, i)
+				}
 
-        sum += histogram[histIndex]
-      }
-    }
+				sum += histogram[histIndex]
+			}
+		}
 
-    total += sum
-    partialSums[i] = total
-  }
+		total += sum
+		partialSums[i] = total
+	}
 
-  return total, partialSums, maxIndex
+	return total, partialSums, maxIndex
 }
 
 func performCut(partialSums []uint, vbox VBox, boundaryVertices []uint8, total uint, maxIndex uint) (VBox, VBox) {
-  lookaheadSums := make([]uint, len(partialSums))
-  for i, partialSum := range partialSums {
-    lookaheadSums[i] = total - partialSum
-  }
+	lookaheadSums := make([]uint, len(partialSums))
+	for i, partialSum := range partialSums {
+		lookaheadSums[i] = total - partialSum
+	}
 
-  markedVertexMin := boundaryVertices[maxIndex*2]
-  markedVertexMax := boundaryVertices[maxIndex*2 + 1]
-  for i := markedVertexMin; i <= markedVertexMax; i++ {
-    if partialSums[i] > (total / 2) {
-      left := i - markedVertexMin
-      right := markedVertexMax - i
+	markedVertexMin := boundaryVertices[maxIndex*2]
+	markedVertexMax := boundaryVertices[maxIndex*2+1]
+	for i := markedVertexMin; i <= markedVertexMax; i++ {
+		if partialSums[i] > (total / 2) {
+			left := i - markedVertexMin
+			right := markedVertexMax - i
 
-      // TODO: replace min & max with whatever is necessary to make this work
-      if left <= right {
-        division := math.Min(markedVertexMax, i + right / 2)
-      } else {
-        division := math.Max(markedVertexMin, i - left / 2)
-      }
+			// TODO: replace min & max with whatever is necessary to make this work
+			var divisionFloat float64
+			if left <= right {
+				divisionFloat = math.Min(float64(markedVertexMax), float64(i+right/2))
+			} else {
+				divisionFloat = math.Max(float64(markedVertexMin), float64(i-left/2))
+			}
+			division := int(divisionFloat)
+			for partialSums[division-2] == 0 {
+				division++
+			}
 
-      for partialSums[d2] == 0 {
-        division++
-      }
+			count := lookaheadSums[division]
 
-      count := lookaheadSum[division]
+			for count == 0 && partialSums[division-1] != 0 {
+				division -= 1
+				//count := lookaheadSums[division]
+			}
 
-      for count == 0 && partialSums[division-1] != 0 {
-        count := lookaheadSum[--division]
-      }
-
-      switch maxIndex {
-      case 0:
-        vbox1 := VBox(vbox.Rmin, division, vbox.Gmin, vbox.Gmax, vbox.Bmin, vbox.Bmax, vbox.Histogram)
-        vbox2 := VBox(division + 1, vbox.Rmax, vbox.Gmin, vbox.Gmax, vbox.Bmin, vbox.Bmax, vbox.Histogram)
-        return vbox1, vbox2
-      case 1:
-        vbox1 := VBox(vbox.Rmin, vbox.Rmax, vbox.Gmin, division, vbox.Bmin, vbox.Bmax, vbox.Histogram)
-        vbox2 := VBox(vbox.Rmin, vbox.Rmax, division + 1, vbox.Gmax, vbox.Bmin, vbox.Bmax, vbox.Histogram)
-        return vbox1, vbox2
-      case 2:
-        vbox1 := VBox(vbox.Rmin, vbox.Rmax, vbox.Gmin, vbox.Gmax, vbox.Bmin, division, vbox.Histogram)
-        vbox2 := VBox(vbox.Rmin, vbox.Rmax, vbox.Gmin, vbox.Gmax, division + 1, vbox.Bmax, vbox.Histogram)
-        return vbox1, vbox2
-      }
-    }
-  }
+			switch maxIndex {
+			case 0:
+				vbox1 := VBox{vbox.Rmin, uint8(division), vbox.Gmin, vbox.Gmax, vbox.Bmin, vbox.Bmax, vbox.Histogram}
+				vbox2 := VBox{uint8(division + 1), vbox.Rmax, vbox.Gmin, vbox.Gmax, vbox.Bmin, vbox.Bmax, vbox.Histogram}
+				return vbox1, vbox2
+			case 1:
+				vbox1 := VBox{vbox.Rmin, vbox.Rmax, vbox.Gmin, uint8(division), vbox.Bmin, vbox.Bmax, vbox.Histogram}
+				vbox2 := VBox{vbox.Rmin, vbox.Rmax, uint8(division + 1), vbox.Gmax, vbox.Bmin, vbox.Bmax, vbox.Histogram}
+				return vbox1, vbox2
+			case 2:
+				vbox1 := VBox{vbox.Rmin, vbox.Rmax, vbox.Gmin, vbox.Gmax, vbox.Bmin, uint8(division), vbox.Histogram}
+				vbox2 := VBox{vbox.Rmin, vbox.Rmax, vbox.Gmin, vbox.Gmax, uint8(division + 1), vbox.Bmax, vbox.Histogram}
+				return vbox1, vbox2
+			}
+		}
+	}
+	return VBox{}, VBox{}
 }
--- primary_color/primary_color.go
+++ primary_color/primary_color.go
@@ -1,105 +1,105 @@
 package primaryColor
 
 import (
-  "fmt"
-  "image"
-  "image/draw"
-  "container/heap"
+	"container/heap"
+	"fmt"
+	"image"
+	"image/draw"
 )
 
 type Rgb struct {
-  R uint8
-  G uint8
-  B uint8
+	R uint8
+	G uint8
+	B uint8
 }
 
 const (
-  minTransparency uint8 = 125
-  maxOpaqueVal uint8 = 250
-  paletteSize uint8 = 10
+	minTransparency uint8 = 125
+	maxOpaqueVal    uint8 = 250
+	paletteSize     uint8 = 10
 
-  sigbits uint = 5
-  rshift uint = 8 - sigbits
+	sigbits uint = 5
+	rshift  uint = 8 - sigbits
 )
 
 func PrimaryColor(img image.Image) {
-  rgbaImg := image.NewRGBA(img.Bounds())
-  draw.Draw(rgbaImg, rgbaImg.Bounds(), img, image.ZP, draw.Src)
+	rgbaImg := image.NewRGBA(img.Bounds())
+	draw.Draw(rgbaImg, rgbaImg.Bounds(), img, image.ZP, draw.Src)
 
-  pixelArray := constructPixelArray(rgbaImg)
-  quantize(pixelArray, paletteSize)
+	pixelArray := constructPixelArray(rgbaImg)
+	quantize(pixelArray, paletteSize)
 }
 
 func quantize(pixelArray []Rgb, size uint8) ([]Rgb, error) {
-  result := make([]Rgb, size)
+	result := make([]Rgb, size)
 
-  if size < 2 || size > 255 {
-    return result, fmt.Errorf("Invalid size: cannot quantize image into %d colors", size)
-  }
+	if size < 2 || size > 255 {
+		return result, fmt.Errorf("Invalid size: cannot quantize image into %d colors", size)
+	}
 
-  histogram := constructHistogram(pixelArray)
-  fmt.Println(histogram)
+	histogram := constructHistogram(pixelArray)
+	fmt.Println(histogram)
 
-  // TODO: take histogram and perform median cut
-  vbox := constructVBox(pixelArray, histogram)
+	// TODO: take histogram and perform median cut
+	//vbox := constructVBox(pixelArray, histogram)
 
-  return result, nil
+	return result, nil
 }
 
 func iterate(queue CountPriorityQueue, histogram []uint, iterations int) {
-  numColors := 0
-  for i := 0; i < iterations; i++ {
-    vbox := heap.Pop(&queue).(*VBox)
-    if vbox.Count() > 0 {
-      heap.Push(&queue, &vbox)
-    } else {
-      vbox1, vbox2 := applyMedianCut(histogram, vbox)
+	numColors := 0
+	for i := 0; i < iterations; i++ {
+		vbox := heap.Pop(&queue).(*VBox)
+		if vbox.Count() > 0 {
+			heap.Push(&queue, &vbox)
+		} else {
+			vbox1, vbox2 := MedianCut(histogram, *vbox)
 
-      heap.Push(&queue, &vbox1)
-      heap.Push(&queue, &vbox2)
-      numColors++
-    }
-  }
+			heap.Push(&queue, &vbox1)
+			heap.Push(&queue, &vbox2)
+			numColors++
+		}
+	}
 }
 
-func constructHistogram(pixelArray []Rgb) []uint{
-  size := 1 << (3 * sigbits)
-  histogram := make([]uint, size)
+func constructHistogram(pixelArray []Rgb) []uint {
+	size := 1 << (3 * sigbits)
+	histogram := make([]uint, size)
 
-  for _, pixel := range pixelArray {
-    i := ColorIndex(pixel.R >> rshift, pixel.G >> rshift, pixel.B >> rshift)
-    histogram[i]++
-  }
+	for _, pixel := range pixelArray {
+		i := ColorIndex(pixel.R>>rshift, pixel.G>>rshift, pixel.B>>rshift)
+		histogram[i]++
+	}
 
-  return histogram
+	return histogram
 }
 
 func constructPixelArray(img *image.RGBA) []Rgb {
-  rgbaImg := image.NewRGBA(img.Bounds())
-  draw.Draw(rgbaImg, rgbaImg.Bounds(), img, image.ZP, draw.Src)
+	rgbaImg := image.NewRGBA(img.Bounds())
+	draw.Draw(rgbaImg, rgbaImg.Bounds(), img, image.ZP, draw.Src)
 
-  pixelArray := make([]Rgb, 0, 50)
-  var rgbVal = Rgb{}
+	pixelArray := make([]Rgb, 0, 50)
+	var rgbVal = Rgb{}
 
-  for i, pix := range rgbaImg.Pix {
-    switch i % 4 {
-    case 0:
-      rgbVal.R = pix
-    case 1:
-      rgbVal.G = pix
-    case 2:
-      rgbVal.B = pix
-    case 3:
-      if pix >= minTransparency && isOpaque(rgbVal) {
-        pixelArray = append(pixelArray, rgbVal)
-        rgbVal = Rgb{}
-      }
-    }
-  }
+	for i, pix := range rgbaImg.Pix {
+		switch i % 4 {
+		case 0:
+			rgbVal.R = pix
+		case 1:
+			rgbVal.G = pix
+		case 2:
+			rgbVal.B = pix
+		case 3:
+			if pix >= minTransparency && isOpaque(rgbVal) {
+				pixelArray = append(pixelArray, rgbVal)
+				rgbVal = Rgb{}
+			}
+		}
+	}
 
-  return pixelArray
+	return pixelArray
 }
 
 func isOpaque(r Rgb) bool {
-  return r.R <= maxOpaqueVal && r.G <= maxOpaqueVal && r.B <= maxOpaqueVal
+	return r.R <= maxOpaqueVal && r.G <= maxOpaqueVal && r.B <= maxOpaqueVal
 }
